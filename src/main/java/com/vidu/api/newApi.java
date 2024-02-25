a0e72242127a140ded06c9055607a46bb82471f3
admin <admin@192.168.56.2>
2024-02-25 22:30:22 +0700

    25 02

--- src/main/java/com/vidu/api/newApi.java
+++ src/main/java/com/vidu/api/newApi.java
@@ -1,5 +1,9 @@
 package com.vidu.api;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +14,11 @@ public class newApi {
 	@GetMapping("/test")
 	public String testApi() {
 		
-		
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+		driver.get("https://live.nowgoal13.com/");
+	   driver.manage().window().maximize();
 		
 		return "success" ;
 	}
