95378b054d7f19a3f4319b0d59a3b50f5560dcf8
admin <admin@192.168.56.2>
2024-02-25 22:58:58 +0700

    10 58

--- src/main/java/com/vidu/api/newApi.java
+++ src/main/java/com/vidu/api/newApi.java
@@ -1,7 +1,10 @@
 package com.vidu.api;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +15,17 @@ public class newApi {
 	
 	
 	@GetMapping("/test")
-	public String testApi() {
+	public List<Long> testApi() {
 		
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
 		driver.get("https://live.nowgoal13.com/");
 	   driver.manage().window().maximize();
-		
-		return "success" ;
+	   List<Long> ids = driver.findElements(By.xpath("//tr[@class='tds']")).stream()
+				.map(element1 -> element1.getAttribute("id")).map(e -> e.substring(4, e.length())).map(e -> Long.parseLong(e))
+				.collect(Collectors.toList());
+		return ids ;
 	}
 
 }
