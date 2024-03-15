122270692d4a221fb1e01f737f0aba21afa38757
admin <admin@192.168.56.2>
2024-03-15 20:00:10 +0700

    9999

--- src/main/java/com/vidu/api/newApi.java
+++ src/main/java/com/vidu/api/newApi.java
@@ -1,12 +1,5 @@
 package com.vidu.api;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +10,14 @@ public class newApi {
 	@GetMapping("/test3")
 	public String testApi() {
 		
-		System.setProperty("webdriver.chrome.driver","D:\\chromedriver-win64/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
-		driver.get("https://live.nowgoal13.com/");
-	   driver.manage().window().maximize();
-	   List<Long> ids = driver.findElements(By.xpath("//tr[@class='tds']")).stream()
-				.map(element1 -> element1.getAttribute("id")).map(e -> e.substring(4, e.length())).map(e -> Long.parseLong(e))
-				.collect(Collectors.toList());
+//		System.setProperty("webdriver.chrome.driver","D:\\chromedriver-win64/chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+//		driver.get("https://live.nowgoal13.com/");
+//	   driver.manage().window().maximize();
+//	   List<Long> ids = driver.findElements(By.xpath("//tr[@class='tds']")).stream()
+//				.map(element1 -> element1.getAttribute("id")).map(e -> e.substring(4, e.length())).map(e -> Long.parseLong(e))
+//				.collect(Collectors.toList());
 		return "that bai" ;
 	}
 
